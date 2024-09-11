f6adceba405bfb95e182e436776aaf8e7a8beb34
amirali511 <amiralivatankhah3@gmail.com>
2024-09-11 23:29:39 +0330

    Improved installer

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "
-  HHH     HHH   EEEEEEEEE   LLL         LLL         III
-  HHH     HHH   EEEEEEEEE   LLL         LLL         III
-  HHH     HHH   EEE         LLL         LLL         III
-  HHHHHHHHHHH   EEEEEEEEE   LLL         LLL         III
-  HHHHHHHHHHH   EEEEEEEEE   LLL         LLL         III
-  HHH     HHH   EEE         LLL         LLL         III
-  HHH     HHH   EEEEEEEEE   LLLLLLLLL   LLLLLLLLL   III
-  HHH     HHH   EEEEEEEEE   LLLLLLLLL   LLLLLLLLL   III
+  HHH     HHH   EEEEEEEEE   LLL         LLL         III   WWW              WWWWWW              WWW   MMMMMM               MMMMMM
+  HHH     HHH   EEEEEEEEE   LLL         LLL         III    WWW            WWW  WWW            WWW    MMM  MMM            MMM MMM
+  HHH     HHH   EEE         LLL         LLL         III     WWW          WWW    WWW          WWW     MMM   MMM          MMM  MMM
+  HHHHHHHHHHH   EEEEEEEEE   LLL         LLL         III      WWW        WWW      WWW        WWW      MMM    MMM        MMM   MMM
+  HHHHHHHHHHH   EEEEEEEEE   LLL         LLL         III       WWW      WWW        WWW      WWW       MMM     MMM      MMM    MMM
+  HHH     HHH   EEE         LLL         LLL         III        WWW    WWW          WWW    WWW        MMM      MMM    MMM     MMM
+  HHH     HHH   EEEEEEEEE   LLLLLLLLL   LLLLLLLLL   III         WWW  WWW            WWW  WWW         MMM       MMM  MMM      MMM
+  HHH     HHH   EEEEEEEEE   LLLLLLLLL   LLLLLLLLL   III          WWWWWW              WWWWWW          MMM        MMMMMM       MMM
 ";
 
 cd src;
