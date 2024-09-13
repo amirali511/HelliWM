832ab5b1d8bd6d08e6fdd14d1b7a0126062ec130
amirali511 <amiralivatankhah3@gmail.com>
2024-09-13 21:40:27 +0330

    Improved installer

--- install.sh
+++ install.sh
@@ -11,10 +11,31 @@ echo "
   HHH     HHH   EEEEEEEEE   LLLLLLLLL   LLLLLLLLL   III          WWWWWW              WWWWWW          MMM        MMMMMM       MMM
 ";
 
-cd src;
-python3 Generator.py;
-awk -f parser.awk;
-meson setup build;
-cp config.h build/config.h;
-ninja -C build;
-sudo ninja -C build install;
+while [[ true ]]; do
+	echo -n "What do you want to do? [y, n, a][install, uninstall, abort]: "
+	read INPUT
+	if [[ $INPUT = y || $INPUT = install ]]; then
+		echo "Installing HelliWM...";
+		cd src;
+		python3 Generator.py;
+		awk -f parser.awk;
+		meson setup build;
+		cp config.h build/config.h;
+		ninja -C build;
+		sudo ninja -C build install;
+		exit;
+	elif [[ $INPUT = n || $INPUT = uninstall ]]; then
+		echo "Uninstalling HelliWM...";
+		cd src;
+		sudo ninja -C build uninstall;
+		rm config.h;
+		rm shortcut.h;
+		rm -R build;
+		exit;
+	elif [[ $INPUT = a || $INPUT = abort ]]; then
+		echo "Aborting...";
+		exit;
+	else
+		echo "Invalid input!";
+	fi
+done
